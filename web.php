1f82e4c8857bf827bbb4ba7be90c00e610fa8a88
Aegeana <48781908+Aegeana@users.noreply.github.com>
2019-05-16 13:28:01 +0800

    Add files via upload

---
+++ web.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta type="utf-8"></meta>
+	<style>
+	.title{
+		font-size:50px;text-align:center;color:black;
+	}
+	.contant{
+		font-size:20px;text-align:center;color:black;
+	}
+	</style>
+</head>
+<body style="margin:0px;">
+	<div class="title">Food Come Close</div>
+	<div class="contant">
+	Welcome to food come close<br/>
+	Hope you can find the restaurant that you want from our website<br/>
+	</div>
+	<?php
+	
+	?>
+</body>
+</html>
