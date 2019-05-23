10cb84d1c464fca671335a5bbb3a4bfc4b3f7d15
Aegeana <48781908+Aegeana@users.noreply.github.com>
2019-05-23 13:37:52 +0800

    Add files via upload

--- web.php
+++ web.php
@@ -9,14 +9,63 @@
 	.contant{
 		font-size:20px;text-align:center;color:black;
 	}
+	.home{
+		color:black;font-size:15px;position:absolute;left:300px;top:120px;cursor:pointer;
+	}
+	.hot{
+		color:black;font-size:15px;position:absolute;left:400px;top:120px;cursor:pointer;
+	}
+	.taipei{
+		color:black;font-size:15px;position:absolute;left:500px;top:120px;cursor:pointer;
+	}
+	.hsinchu{
+		color:black;font-size:15px;position:absolute;left:600px;top:120px;cursor:pointer;
+	}
+	.taichung{
+		color:black;font-size:15px;position:absolute;left:700px;top:120px;cursor:pointer;
+	}
+	.tainan{
+		color:black;font-size:15px;position:absolute;left:800px;top:120px;cursor:pointer;
+	}
+	.kaohsiung{
+		color:black;font-size:15px;position:absolute;left:900px;top:120px;cursor:pointer;
+	}
+	#home:hover{
+		font-size:20px;
+	}
+	#hot:hover{
+		font-size:20px;
+	}
+	#taipei:hover{
+		font-size:20px;
+	}
+	#hsinchu:hover{
+		font-size:20px;
+	}
+	#taichung:hover{
+		font-size:20px;
+	}
+	#tainan:hover{
+		font-size:20px;
+	}
+	#kaohsiung:hover{
+		font-size:20px;
+	}
 	</style>
 </head>
-<body style="margin:0px;">
+<body style="margin:0px;background-color:#FFFAFA">
 	<div class="title">Food Come Close</div>
 	<div class="contant">
 	Welcome to food come close<br/>
 	Hope you can find the restaurant that you want from our website<br/>
 	</div>
+	<div class="home" id="home"><a href="file:///C:/Users/www/webtaipei.html">Home</a></div>
+	<div class="hot" id="hot"><a>Hot</a></div>
+	<div class="taipei" id="taipei"><a>Taipai</a></div>
+	<div class="hsinchu" id="hsinchu"><a>Hsinchu</a></div>
+	<div class="taichung" id="taichung"><a>Taichung</a></div>
+	<div class="tainan" id="tainan"><a>Tainan</a></div>
+	<div class="kaohsiung" id="kaohsiung"><a>Kaohsiung</a></div>
 	<?php
 	
 	?>
